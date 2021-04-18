26fe06ee00a8e8bec703de79702420e8ad161ba6
Dannyvo2 <56869943+Dannyvo1@users.noreply.github.com>
2021-04-18 15:33:53 +0700

    api: localhost:*****//updatecart/<int:code> Execute: update cart status: success

--- Final_WatchShoppingWeb/Final_WatchShoppingWeb/Final_WatchShoppingWeb/carts/carts.py
+++ Final_WatchShoppingWeb/Final_WatchShoppingWeb/Final_WatchShoppingWeb/carts/carts.py
@@ -19,7 +19,7 @@ def Addcart():
         colors = request.form.get('colors')
         product = _session.query(Product).filter(Product.id == product_id).first()
         if 'product_id' in request.form and 'quantity' in request.form and 'colors' in request.form and request.method == "POST":
-            DicItems = {product_id:{'name': product.name, 'price': float(product.price), 'discount': product.discount, 'color': product.colors, 'quantity': quantity, 'image': product.image_1}}
+            DicItems = {product_id:{'name': product.name, 'price': float(product.price), 'discount': product.discount, 'color': product.colors, 'quantity': quantity, 'image': product.image_1, 'colors': product.colors}}
 
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
@@ -53,3 +53,30 @@ def getCart():
     _brands = _session.query(Brand).join(Product, (Brand.id == Product.brand_id)).all()
     _categories = _session.query(Category).join(Product, (Category.id == Product.category_id)).all()
     return render_template('products/carts.html', tax=tax, grandtotal=grandtotal, _brands=_brands, _categories=_categories)
+
+@app.route('/updatecart/<int:code>', methods=['POST'])
+def updatecart(code):
+    if 'Shoppingcart' not in session and len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == "POST":
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    flash('Item is updated')
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+#@app.route('/empty')
+#def empty_cart():
+#    try:
+#        session.clear()
+#        return redirect(url_for('home'))
+#    except Exception as e:
+#        print(e) 
